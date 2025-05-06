de2a7a1c6d6ba98de08b2f4c97616da351d00343
Bharat Dondeti <bharatdondeti@Bharats-MacBook-Pro.local>
2025-05-06 19:45:56 +0100

    second commit

---
+++ frontend/src/pages/blogs.tsx
@@ -0,0 +1,13 @@
+import { BlogCard } from "../components/blogcard"
+
+export const Blog = () => {
+    return <div>
+        <BlogCard
+            authorName={"Bharat"}
+            title={"title of the blog"}
+            content={"Contentm of the blog"}
+            publishedDate={"6th May 2025"} 
+        />
+    </div>
+}
+
